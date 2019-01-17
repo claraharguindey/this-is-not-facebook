6f2c2913f391ee10eae94b3ff2752d2c3be2dc46
clara <clarisaharguindey@gmail.com>
2019-01-17 18:23:14 +0100

    components

---
+++ src/components/UserList/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserList extends Component {
+
+    render() {
+        return (
+            <ul>
+                
+            </ul>
+        );
+    }
+}
+
+export default UserList;
